1969f70ccb6b5df1be1c7dd486390b43cd6a7d65
Darius <153660200+Dariiuuss01@users.noreply.github.com>
2025-11-13 22:57:28 +0100

    Cambios: - He quitado imports que no me hacian falta

--- src/main/java/com/libreria/Controlador/ControladorLibreria.java
+++ src/main/java/com/libreria/Controlador/ControladorLibreria.java
@@ -1,31 +1,21 @@
 package com.libreria.Controlador;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.libreria.Models.Libro;
-import com.libreria.RepoLibros.RepoLibros;
 import com.libreria.Servicio.InterfaceServicioLibro;
-import com.libreria.Servicio.ServicioLibro;
-
-import jakarta.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 
 @RestController
 @RequestMapping("/libreria/libros")
